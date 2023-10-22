266d0a3728f9b21ab5cc2aedb1055a220d7abbef
Duygu Yesiloglu <110973866+duyguyesiloglu@users.noreply.github.com>
2023-10-22 19:59:13 +0300

    AppDelegate.swift

--- Yemek'De/Yemek'De/App/AppDelegate.swift
+++ Yemek'De/Yemek'De/App/AppDelegate.swift
@@ -13,6 +13,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        //Launch Screen durması icin
         Thread.sleep(forTimeInterval: 0.2)
         // Override point for customization after application launch.
         return true
